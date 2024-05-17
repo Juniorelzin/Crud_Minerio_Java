9947c0f7343633d5c92d64442cdfd4d595221021
ESTEVAO LIMAS JUNIOR <estevao_limas-jun@estudante.sesisenai.org.br>
2024-05-16 21:43:17 -0300

    Adicionado CRUD

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         TelaCadastro t = new TelaCadastro();
         t.setContentPane(t.telaPrincipal);
         t.setTitle("Tela Minérios");
-        t.setSize(400,300);
+        t.setSize(800,600);
         t.setVisible(true);
         t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
--- src/TelaCadastro.java
+++ src/TelaCadastro.java
@@ -10,10 +10,30 @@ public class TelaCadastro extends JFrame{
     private JTextField txtValor;
     private JButton btnEnviar;
     private JTextArea textArea;
+    private JButton btnRemover;
+    private JButton btnLocalizar;
     private JButton btnMostrar;
+    private JButton btnEditar;
+
+    private Minerio alvo = null;
+
+    private void mostrarTodos(){
+
+        String mostrar = "";
+        for (int i = 0; i < minerios.size(); i++) {
+
+            mostrar += (" Nome: " + minerios.get(i).getNome() +"\n" + " Valor:  " + minerios.get(i).getValor()+"\n\n");
+        }
+        textArea.setText(mostrar);
+    }
+    private void limparInputs(){
+        txtNome.setText("");
+        txtValor.setText("");
+    }
 
 
     public TelaCadastro() {
+
         btnEnviar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -22,14 +42,71 @@ public class TelaCadastro extends JFrame{
                 Minerio M = new Minerio(nome, valor);
                 minerios.add(M);
 
-                String mostrar = "";
+                mostrarTodos();
+                limparInputs();
+
+            }
+        });
+
+        btnLocalizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String nome = txtNome.getText();
+
                 for (int i = 0; i < minerios.size(); i++) {
+                    String compararNome = minerios.get(i).getNome();
 
-
-                    mostrar += (" Nome: " + minerios.get(i).getNome() +"\n" + " Valor: " + minerios.get(i).getValor()+"\n\n");
+                    if(compararNome.equals(nome)){
+                        alvo = minerios.get(i);
+                        String mostrarNome = alvo.getNome();
+                        String mostrarValor = Integer.toString(alvo.getValor());
+                        txtNome.setText(mostrarNome);
+                        txtValor.setText(mostrarValor);
+                    }
                 }
-                textArea.setText(mostrar);
 
+            }
+        });
+
+        btnEditar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(alvo != null) {
+                    alvo.setNome(txtNome.getText());
+                    alvo.setValor(Integer.parseInt(txtValor.getText()));
+
+                    mostrarTodos();
+                    limparInputs();
+                    alvo = null;
+                }else{
+                    JOptionPane.showMessageDialog(null, "Procura primeiro seu bosta!!");
+                }
+            }
+        });
+
+        btnRemover.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nome = txtNome.getText();
+
+                for (int i = 0; i < minerios.size(); i++) {
+                    String comparar = minerios.get(i).getNome();
+                    System.out.println(comparar);
+                    if(comparar.equals(nome)){
+                        int index = i;
+                        minerios.remove(index);
+                    }
+                }
+                mostrarTodos();
+                limparInputs();
+            }
+        });
+
+        btnMostrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarTodos();
             }
         });
 
